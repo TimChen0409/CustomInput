531fe50d61fc70c6eb187b2a0af08a8cce6b42fa
Tim <59609847+TimChen0409@users.noreply.github.com>
2023-11-04 19:40:07 +0800

    Add files via upload

---
+++ CustomInput/README.md
@@ -0,0 +1,21 @@
+# VitualScroller
+
+A custom component for Blazing fast scrolling of any amount of data
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ CustomInput/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <custom-input-number :name="'inputNumber1'" :value="0" :min="0" :max="10" :step="1" :disabled="false"
+      @onBlur="handleOnBlur" @onChange="handleOnChange">
+    </custom-input-number>
+    <hr>
+    <room-allocation :guest="8" :room="4" @onChange="handleRoomAllocationOnChange">
+    </room-allocation>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount, onMounted, ref, reactive } from "vue";
+import CustomInputNumber from "@/components/CustomInputNumber.vue";
+import RoomAllocation from "@/components/RoomAllocation.vue";
+
+const handleOnBlur = (name, value) => {
+  console.log("onBlur name: " + name);
+  console.log("onBlur value: " + value);
+}
+
+const handleOnChange = (name, value) => {
+  console.log("onChange name: " + name);
+  console.log("onChange value: " + value);
+}
+
+const handleRoomAllocationOnChange = (value) => {
+  console.log(value);
+}
+
+</script>
+
+<style lang="scss" scoped></style>
